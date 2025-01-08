b1228f3f05af9ce19f860adb674f6334fea9de43
aashif000 <mohammedaashif1370@gmail.com>
2025-01-09 03:32:51 +0530

    Updates

--- src/test/setup.ts
+++
@@ -1,9 +0,0 @@
- // If you want to mock APIs with MSW (Mock Service Worker)
-import { QueryClient, QueryClientProvider } from 'react-query';
-import '@testing-library/jest-dom'; // Import jest-dom to enable additional matchers
-
-// Create a new QueryClient instance
-const queryClient = new QueryClient();
-
-// Global setup for the tests
-globalThis.queryClient = queryClient;
